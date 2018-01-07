1144b00dd5e50c26d02709280e91401c82fbee12
Ruben Bernardo <rbernardo.span@gmail.com>
2018-01-07 23:02:18 +0100

    servicio firefase ok

--- src/app/providers/informacion.service.ts
+++ src/app/providers/informacion.service.ts
@@ -6,14 +6,34 @@ export class InformacionService {
 
   info:any = {}
   cargada:boolean = false;
+  cargadaSobreNosotros:boolean = false;
+  equipo:any[] = [];
 
   constructor(public http:Http) {
 
+    this.carga_info ();
+    this.carga_sobre_nosotros ();
+
+  }
+
+  public carga_info () {
+
     this.http.get("assets/data/info.pagina.json")
       .subscribe ( data => {
-          console.log ( data.json() );
           this.cargada = true;
           this.info = data.json();
+      })
+
+
+  }
+
+  public carga_sobre_nosotros () {
+
+    this.http.get("https://portfolio-77e1b.firebaseio.com/equipo.json")
+      .subscribe ( data => {
+          console.log (data.json());
+          this.cargadaSobreNosotros = true;
+          this.equipo = data.json();
       })
 
   }
