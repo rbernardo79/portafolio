c191c098b5618109526eb00526646274229197e9
Ruben Bernardo <rbernardo.span@gmail.com>
2018-01-10 23:51:22 +0100

    buscador finalizado

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+
+import { ProductosService } from "../../providers/productos.service";
+
 
 @Component({
   selector: 'app-search',
@@ -6,5 +10,24 @@ import { Component } from '@angular/core';
 })
 export class SearchComponent  {
 
-  
+  productos:any[] = [];
+  termino:string = undefined;
+
+  constructor ( private _ar:ActivatedRoute, private _ps:ProductosService ) {
+
+    _ar.params.subscribe ( parametros => {
+
+      this.termino = parametros['termino'];
+      console.log ( this.termino );
+      _ps.buscar_producto (this.termino);
+
+    })
+
+  }
+
+
+
+
+
+
 }
--- src/app/providers/productos.service.ts
+++ src/app/providers/productos.service.ts
@@ -5,6 +5,7 @@ import { Http } from "@angular/http";
 export class ProductosService {
 
   productos:any[] = [];
+  productos_filtrado:any[] = [];
   cargando:boolean = false;
   producto:any;
 
@@ -14,23 +15,56 @@ export class ProductosService {
 
   }
 
+  buscar_producto ( termino:string) {
+
+    if ( this.productos.length === 0 ) {
+      this.cargar_productos().then ( ()=> {
+        // terminó la carga
+        this.filtrar_productos (termino);
+      })
+    } else {
+      this.filtrar_productos (termino);
+    }
+
+  }
+
+  private filtrar_productos ( termino:string ){
+
+    this.productos_filtrado = [];
+    termino = termino.toLowerCase();
+
+    this.productos.forEach ( prod => {
+
+      if ( prod.categoria.indexOf (termino) >=0 ||
+              prod.titulo.toLowerCase().indexOf (termino) >=0) {
+        this.productos_filtrado.push( prod )
+      }
+    })
+
+    console.log(this.productos_filtrado);
+
+
+  }
+
   public cargar_productos () {
 
     this.cargando = true;
 
-    if ( this.productos.length === 0 ) {
+    let promesa = new Promise ( (resolve,reject)  => {
 
-      this.http.get ("https://portfolio-77e1b.firebaseio.com/productos_idx.json")
-        .subscribe ( res => {
+    this.http.get ("https://portfolio-77e1b.firebaseio.com/productos_idx.json")
+      .subscribe ( res => {
 
-          setTimeout ( () => {
-            this.productos = res.json();
-            this.cargando = false;
-          },1500 );
+        setTimeout ( () => {
+          this.productos = res.json();
+          this.cargando = false;
+          resolve();
+        },1500 );
 
-        })
+      })
 
-    }
+    })
+    return promesa;
 
   }
 
